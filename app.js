a1663a18f74b106be64d2207ed131364919ad8e2
Kal-PC-FIXE <akallavero@gmail.com>
2021-06-17 23:37:40 +0200

    COD Menu déroulant mobile

--- app.js
+++ app.js
@@ -4,12 +4,12 @@
 
 // Affichage menu sur mobile
 const btnBurgerElt = document.querySelector(".header__burger");
-const navUlElt = document.querySelector(".header__nav__ul");
+const navbarMobileElt = document.querySelector(".navbar-mobile");
 const burgerTiretElt = document.querySelector(".burger-tiret");
 
 btnBurgerElt.addEventListener("click", () => {
   burgerTiretElt.classList.toggle("burger-tiret--anim");
-  navUlElt.classList.toggle("active-menu-deroulant");
+  navbarMobileElt.classList.toggle("active-menu-deroulant");
 });
 
 // Animation bouton burger
