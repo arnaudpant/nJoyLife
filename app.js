9693d193fa3ad622bea6967b741d42a5aefe93a4
Kal-PC-FIXE <akallavero@gmail.com>
2021-06-17 22:41:55 +0200

    COD anim bouton burger

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const btnBurgerElt = document.querySelector('.header__burger');
-const navUlElt = document.querySelector('.header__nav__ul');
+// *********************************
+// HEADER
+// *********************************
 
-btnBurgerElt.addEventListener('click', () => {
-  navUlElt.classList.toggle('active-menu-deroulant')
-})
+// Affichage menu sur mobile
+const btnBurgerElt = document.querySelector(".header__burger");
+const navUlElt = document.querySelector(".header__nav__ul");
+const burgerTiretElt = document.querySelector(".burger-tiret");
+
+btnBurgerElt.addEventListener("click", () => {
+  burgerTiretElt.classList.toggle("burger-tiret--anim");
+  navUlElt.classList.toggle("active-menu-deroulant");
+});
+
+// Animation bouton burger
