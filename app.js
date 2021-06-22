7469049374260dd0821e00dcde31fd1f201652c2
Kal-PC-FIXE <akallavero@gmail.com>
2021-06-22 22:44:05 +0200

    MOD navbar mobile changement titre

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ const navbarMobileContainerElt = document.querySelector(
   ".navbar-mobile__container"
 );
 const navbarTitleElt = document.querySelector(".header__title");
+const navbarProfessionElt = document.querySelector(".header__profession");
 
 btnBurgerElt.addEventListener("click", () => {
   burgerTiretElt.classList.toggle("burger-tiret--anim");
   navbarMobileElt.classList.toggle("active-menu-deroulant");
   navbarMobileContainerElt.classList.toggle("active-menu-deroulant");
-  // navbarTitleElt.textContent = "NATUROPATHE";
+  navbarTitleElt.classList.toggle("active-menu-deroulant");
+  navbarProfessionElt.classList.toggle("active-menu-deroulant");
 });
