e1205ee8575c1e714e12be0cb26134583d5c652d
Kal-PC-FIXE <akallavero@gmail.com>
2021-06-22 22:17:51 +0200

    MOD navbar mobile

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ const burgerTiretElt = document.querySelector(".burger-tiret");
 const navbarMobileContainerElt = document.querySelector(
   ".navbar-mobile__container"
 );
+const navbarTitleElt = document.querySelector(".header__title");
 
 btnBurgerElt.addEventListener("click", () => {
   burgerTiretElt.classList.toggle("burger-tiret--anim");
   navbarMobileElt.classList.toggle("active-menu-deroulant");
   navbarMobileContainerElt.classList.toggle("active-menu-deroulant");
+  // navbarTitleElt.textContent = "NATUROPATHE";
 });
