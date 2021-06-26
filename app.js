72893afc875d106b2f997684545aa2c4b807d07e
Kal-PC-FIXE <akallavero@gmail.com>
2021-06-26 23:00:27 +0200

    HEADER Menu déoulant fin codage

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 // Affichage menu sur mobile
 const btnBurgerElt = document.querySelector(".header-burger");
-// const navbarMobileElt = document.querySelector(".header-mobile");
+const navbarMobileElt = document.querySelector(".header-mobile");
 const burgerTiretElt = document.querySelector(".burger-tiret");
 // const navbarMobileContainerElt = document.querySelector(
 //   ".navbar-mobile__container"
@@ -14,7 +14,7 @@ const headerProfessionElt = document.querySelector(".header-profession");
 
 btnBurgerElt.addEventListener("click", () => {
   burgerTiretElt.classList.toggle("burger-tiret--anim");
-  // navbarMobileElt.classList.toggle("active-menu-deroulant");
+  navbarMobileElt.classList.toggle("active-menu-deroulant");
   // navbarMobileContainerElt.classList.toggle("active-menu-deroulant");
   headerNameElt.classList.toggle("active-menu-deroulant");
   headerProfessionElt.classList.toggle("active-menu-deroulant");
