9ca245af541c3fcd614636ccdc416c4c6429d3e5
Kal-PC-FIXE <akallavero@gmail.com>
2021-06-28 18:32:02 +0200

    28/06 Commit 2 - Retouche Header desktop

--- app.js
+++ app.js
@@ -6,16 +6,13 @@
 const btnBurgerElt = document.querySelector(".header-burger");
 const navbarMobileElt = document.querySelector(".header-mobile");
 const burgerTiretElt = document.querySelector(".burger-tiret");
-// const navbarMobileContainerElt = document.querySelector(
-//   ".navbar-mobile__container"
-// );
+
 const headerNameElt = document.querySelector(".header-name");
 const headerProfessionElt = document.querySelector(".header-profession");
 
 btnBurgerElt.addEventListener("click", () => {
   burgerTiretElt.classList.toggle("burger-tiret--anim");
   navbarMobileElt.classList.toggle("active-menu-deroulant");
-  // navbarMobileContainerElt.classList.toggle("active-menu-deroulant");
   headerNameElt.classList.toggle("active-menu-deroulant");
   headerProfessionElt.classList.toggle("active-menu-deroulant");
 });
