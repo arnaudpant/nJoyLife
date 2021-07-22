82884164713b1c8aa187b3df522572c9682e0c21
Kallavero <akallavero@gmail.com>
2021-07-22 22:47:14 +0200

    22/07 - 02 - Modif btn et anim header et navbar

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const btnBurgerElt = document.querySelector(".header-burger");
 const navbarMobileElt = document.querySelector(".header-mobile");
 const burgerTiretElt = document.querySelector(".burger-tiret");
 
+const lienNavbarAboutElt = document.querySelectorAll(".header-mobile__liens__li")
+
 const headerNameElt = document.querySelector(".header-name");
 const headerProfessionElt = document.querySelector(".header-profession");
 
@@ -16,6 +18,21 @@ btnBurgerElt.addEventListener("click", () => {
   headerNameElt.classList.toggle("active-menu-deroulant");
   headerProfessionElt.classList.toggle("active-menu-deroulant");
 });
+
+for (let i = 0; i < lienNavbarAboutElt.length; i++) {
+    lienNavbarAboutElt[i].addEventListener ('click', () => {
+      burgerTiretElt.classList.toggle("burger-tiret--anim");
+      navbarMobileElt.classList.toggle("active-menu-deroulant");
+      headerNameElt.classList.toggle("active-menu-deroulant");
+      headerProfessionElt.classList.toggle("active-menu-deroulant");
+  })
+}
+
+
+// lienNavbarAboutElt[0].addEventListener ('click', ()=>{
+//   navbarMobileElt.classList.toggle("active-menu-deroulant");
+// });
+
 
 // *********************************
 // SECTION 1
